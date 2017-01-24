e1996900870d3c3465b913fb284cf1392d7bde9b
Alain <alain@unai>
2017-01-24 13:55:23 +0100

    Tag name update to unique=true

--- models/Behigorri/Entities/Tag.php
+++ models/Behigorri/Entities/Tag.php
@@ -24,7 +24,7 @@ class Tag
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=false)
+     * @ORM\Column(type="string", length=255, nullable=false, unique=true)
      */
     private $name;
 
