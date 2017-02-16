3075f2a50b6554cc2003dfe2e9404b9622465aba
Alain <alain@unai>
2017-02-16 14:03:28 +0100

    modifying lenguague menu

--- resources/views/index/menu.blade.php
+++ resources/views/index/menu.blade.php
@@ -42,6 +42,26 @@
 </ul>
 </li>
 </ul>
+<ul class="nav navbar-right top-nav">
+  <li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> {{ trans('translations.languague') }} <b class="caret"></b></a>
+    <ul class="dropdown-menu">
+      <li>
+        <a href="/edit/profile"><i class="fa fa-fw fa-user"></i> {{ trans('translations.editprofile') }}</a>
+      </li>
+      <!--<li>
+      <a href="#"><i class="fa fa-fw fa-envelope"></i> Inbox</a>
+    </li>
+    <li>
+    <a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a>
+  </li>-->
+  <li class="divider"></li>
+  <li>
+    <a href="{{ route('logout') }}"><i class="fa fa-fw fa-power-off"></i> {{ trans('translations.logout') }}</a>
+  </li>
+</ul>
+</li>
+</ul>
 
 <!--
 <nav class="navbar navbar-default navbar-fixed-top">
