6a3892a16875a503cb58f02aa1261fb462acb235
thevirus <thevirus@mordor>
2017-02-20 14:33:37 +0100

    language buttons translation and toggle menu edited v2

--- resources/views/index/menu.blade.php
+++ resources/views/index/menu.blade.php
@@ -14,20 +14,6 @@ $locale =App::getLocale(); ?>
 <div  class="collapse navbar-collapse navbar-ex1-collapse">
 <ul class="nav navbar-right top-nav">
   <li class="dropdown">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> {{ $user->getEmail() }} <b class="caret"></b></a>
-    <ul class="dropdown-menu">
-      <li>
-        <a href="/edit/profile"><i class="fa fa-fw fa-user"></i> {{ trans('translations.editprofile') }}</a>
-      </li>
-  <li class="divider"></li>
-  <li>
-    <a href="{{ route('logout') }}"><i class="fa fa-fw fa-power-off"></i> {{ trans('translations.logout') }}</a>
-  </li>
-</ul>
-</li>
-</ul>
-<ul class="nav navbar-right top-nav">
-  <li class="dropdown">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-letter"></i> {{ trans('translations.selectLanguage') }} <b class="caret"></b></a>
     <ul class="dropdown-menu">
 
@@ -42,7 +28,20 @@ $locale =App::getLocale(); ?>
         </li>
         @endif
         @endforeach
-</ul>
+  </ul>
+  </li>
+  <li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> {{ $user->getEmail() }} <b class="caret"></b></a>
+    <ul class="dropdown-menu">
+      <li>
+        <a href="/edit/profile"><i class="fa fa-fw fa-user"></i> {{ trans('translations.editprofile') }}</a>
+      </li>
+      <li class="divider"></li>
+      <li>
+        <a href="{{ route('logout') }}"><i class="fa fa-fw fa-power-off"></i> {{ trans('translations.logout') }}</a>
+      </li>
+    </ul>
 </li>
 </ul>
+
 </div>
