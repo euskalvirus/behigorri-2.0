5e15e0f65b076f785a4d131104482e765835bd13
thevirus <thevirus@mordor>
2017-02-21 16:10:10 +0100

    updating theme from sb-admin to sb-admim2 v8

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -80,6 +80,18 @@
 
         <!-- Custom Theme JavaScript -->
         <script src="{{ asset('js/sb-admin-2.js') }}"></script>
+
+        <!-- DataTables JavaScript -->
+        <script src="../vendor/datatables/js/jquery.dataTables.min.js"></script>
+        <script src="../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+        <script src="../vendor/datatables-responsive/dataTables.responsive.js"></script>
+        <script>
+        $(document).ready(function() {
+            $('#dataTables-example').DataTable({
+                responsive: true
+            });
+        });
+        </script>
       </body>
 
 </html>
